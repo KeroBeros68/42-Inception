de1fe6b78d536ac5aef7e0124accb9c116fca1f2
Kevin Bertrand <kevinbertrand68.pro@gmail.com>
2026-07-22 18:11:22 +0000

    update

--- srcs/mariaDB/tools/entrypoint.sh
+++ srcs/mariaDB/tools/entrypoint.sh
@@ -2,8 +2,44 @@
 
 set -e
 
-if [ "$1" = "mariadb" ]; then
-	print("ok")
+if [ "$1" = 'mariadbd' ]; then
+
+    if [ -z "$MARIADB_PORT" ]; then
+        echo "[ERROR]: Missing MARIADB_PORT environment variable." >&2
+        exit 1
+    fi
+
+    if [ ! -f "/var/lib/mysql/.initialized" ]; then
+        
+        MARIADB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+        MARIADB_PASSWORD=$(cat /run/secrets/db_password)
+
+        if [ -z "$MARIADB_DATABASE" ] || [ -z "$MARIADB_ROOT_PASSWORD" ] || [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_PASSWORD" ]; then
+            echo "[ERROR]: Missing database environment." >&2
+            exit 1
+        fi
+
+        echo "Initializing MariaDB..."
+
+        chown -R mysql:mysql /var/lib/mysql
+
+        mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+        mariadbd --user=mysql --datadir=/var/lib/mysql --bootstrap <<EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+        touch /var/lib/mysql/.initialized
+
+        echo "[OK] MariaDB initialized successfully."
+    fi
+
+    set -- "$@" --user=mysql --bind-address=0.0.0.0 --port="$MARIADB_PORT"
 fi
 
 exec "$@"
--- srcs/nginx/tools/entrypoint.sh
+++ srcs/nginx/tools/entrypoint.sh
@@ -5,11 +5,12 @@ set -e
 if [ "$1" = "nginx" ]; then
 
     if [ -z "$DOMAIN_NAME" ] || [ -z "$NGINX_PORT" ] || [ -z "$WORDPRESS_PORT" ]; then
-        echo "Error: Missing DOMAIN_NAME, NGINX_PORT and/or WP_PORT environment variable(s)." >&2
+        echo "[ERROR]: Missing DOMAIN_NAME, NGINX_PORT and/or WP_PORT environment variable(s)." >&2
         exit 1
     fi
 
-    echo "[INFO] Configuring NGINX for domain: $DOMAIN_NAME"
+    echo "Configuring NGINX for: $DOMAIN_NAME"
+
     sed -i "s/__DOMAIN_NAME__/$DOMAIN_NAME/g" /etc/nginx/nginx.conf
     sed -i "s/__NGINX_PORT__/$NGINX_PORT/g" /etc/nginx/nginx.conf
     sed -i "s/__WORDPRESS_PORT__/$WORDPRESS_PORT/g" /etc/nginx/nginx.conf
@@ -17,7 +18,7 @@ if [ "$1" = "nginx" ]; then
 	    CERTS_DIR="/etc/nginx/ssl"
 
     if [ ! -f "$CERTS_DIR/$DOMAIN_NAME.crt" ]; then
-        echo "[INFO] Generating self-signed SSL certificate..."
+        echo "Generating self-signed SSL certificate..."
         
         mkdir -p "$CERTS_DIR"
 
@@ -26,7 +27,7 @@ if [ "$1" = "nginx" ]; then
             -out "$CERTS_DIR/$DOMAIN_NAME.crt" \
             -subj "/C=FR/ST=GrandEst/L=Mulhouse/O=42/OU=Inception/CN=$DOMAIN_NAME"
             
-        echo "[INFO] SSL certificate generated successfully."
+        echo "[OK] SSL certificate generated successfully."
     fi
 
 fi
