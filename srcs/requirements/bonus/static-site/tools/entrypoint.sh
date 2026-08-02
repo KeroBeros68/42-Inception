9456dcc0dad1fed53976193bdaea369276b6d817
Kevin Bertrand <kevinbertrand68.pro@gmail.com>
2026-08-02 03:04:16 +0200

    update

--- srcs/requirements/bonus/static-site/tools/entrypoint.sh
+++ srcs/requirements/bonus/static-site/tools/entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [ "$1" = "httpd" ]; then
+if [ "$1" = "nginx" ]; then
 
     if [ -z "$STATIC_SITE_PORT" ]; then
         echo "[ERROR]: Missing STATIC_SITE_PORT environment variable." >&2
         exit 1
     fi
 
-    set -- httpd -f -p "$STATIC_SITE_PORT" -h /var/www/html
+    sed -i "s/__STATIC_SITE_PORT__/$STATIC_SITE_PORT/g" /etc/nginx/nginx.conf
 fi
 
 exec "$@"
